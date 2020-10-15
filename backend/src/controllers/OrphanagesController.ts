29f71697b2da2fe33ca5c450b26a3951cf2be4b9
s7thiago <thyagosousasilva@gmail.com>
2020-10-14 22:19:58 -0300

    Dia 2 - Listando e detalhando um orfanato

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm';
+
 import Orphanage from '../models/Orphanage';
 
 export default {
+
+    async show(request: Request, response: Response) {
+        // Pegando o id dos Route params 
+        const { id } = request.params;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.findOneOrFail(id);
+        return response.json(orphanages);
+    },
+
+    async index(request: Request, response: Response) {
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanagesRepository.find();
+        return response.json(orphanage);
+    },
+
     async create(request: Request, response: Response) {
         // Desestruturando o corpo da requisição par apegar os dados do orfanato que será cadastrado
         const {
