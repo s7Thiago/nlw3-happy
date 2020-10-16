0b5b93027395e5f9ddf057fff2e2cf4a1012b025
s7thiago <thyagosousasilva@gmail.com>
2020-10-15 23:17:31 -0300

    Dia 3 - Abstraindo componentes

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -4,19 +4,9 @@ import mapMarker from '../images/map-marker.svg';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
-
-import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
-import 'leaflet/dist/leaflet.css'
-
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-});
+import mapIcon from '../utils/mapIcon';
 
 function OrphanagesMap() {
     return (
